203997238fdfa778f43c36f36c9224fa99780bad
Ren Zhu <rockmanexe3@hotmail.com>
2020-05-01 00:56:28 -0400

    ALDS1_3_B queue

---
+++ alds1/alds1_3/queue.py
@@ -0,0 +1,23 @@
+import sys
+from collections import deque
+
+_, quantum = tuple(int(n) for n in input().split())
+
+q = deque([(lambda x,y: [x, int(y)])(*tuple(line.strip().split())) \
+        for line in sys.stdin.readlines()])
+
+current_time = 0
+task_end = deque()
+
+while q:
+    task_pair = q.popleft()
+    if task_pair[1] <= quantum:
+        current_time += task_pair[1]
+        task_end.append([task_pair[0], current_time])
+    else:
+        task_pair[1] -= quantum
+        q.append(task_pair)
+        current_time += quantum
+
+for pair in task_end:
+    print('{} {}'.format(*pair))
